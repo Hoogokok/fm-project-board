6156e9a29b0e5a8220e24b4c4130cfc7d814cf15
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2023-04-02 23:01:11 +0900

    #16-시큐리티 모든 경로에서 접근 가능하게 바꿈

---
+++ fm-project-board/src/test/java/com/fastcampus/projectboard/controller/AuthControllerTest.java
@@ -0,0 +1,34 @@
+package com.fastcampus.projectboard.controller;
+
+import com.fastcampus.projectboard.config.SecurityConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
+@Import({SecurityConfig.class})
+@DisplayName("View 컨트롤러 -인증")
+class AuthControllerTest {
+    private final MockMvc mvc;
+
+    public AuthControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view][GET] 로그인 - 정상 호출")
+    @Test
+    void givenNothing_whenLoggingIn_thenReturnsLoginView() throws Exception {
+
+
+        mvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+    }
+}
