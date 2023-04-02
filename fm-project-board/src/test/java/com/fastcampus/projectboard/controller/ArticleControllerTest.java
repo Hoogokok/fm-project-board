9a04819aaa3d423e412dd1c6dbe0cb5f5301afc9
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2023-04-02 12:19:29 +0900

    #36-view:구현 전 테스트 코드에 disabled 처리
    테스트 코드가 통과하는 구현을 개발 중이기에 disabled 처리를 하고, 세부 테스트 명세를 바꾸었다.

--- fm-project-board/src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ fm-project-board/src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -1,5 +1,6 @@
 package com.fastcampus.projectboard.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +15,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 -게시글")
-@WebMvcTest
+@WebMvcTest(ArticleControllerTest.class)
 class ArticleControllerTest {
     private final MockMvc mockMvc;
 
@@ -22,6 +23,7 @@ class ArticleControllerTest {
         this.mockMvc = mockMvc;
     }
 
+    @Disabled("개발 중")
     @DisplayName("[view][GET] 게시글 리스트 - 정상 호출")
     @Test
     void givenNothing_whenRequestArticlesView_thenReturnsArticlesView() throws Exception {
@@ -30,8 +32,10 @@ class ArticleControllerTest {
         mockMvc.perform(get("/articles"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
     }
+    @Disabled("개발 중")
 
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
@@ -41,8 +45,11 @@ class ArticleControllerTest {
         mockMvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/detail"))
+                .andExpect(model().attributeExists("articleComments"))
                 .andExpect(model().attributeExists("article"));
     }
+    @Disabled("개발 중")
 
     @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상 호출")
     @Test
@@ -51,8 +58,10 @@ class ArticleControllerTest {
 
         mockMvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("article/search"));
     }
+    @Disabled("개발 중")
 
     @DisplayName("[view][GET] 게시글 해시태그 검색 페이지 - 정상 호출")
     @Test
@@ -61,6 +70,7 @@ class ArticleControllerTest {
 
         mockMvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles/search-hashtag"));
     }
 }
